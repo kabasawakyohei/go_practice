d351572d916c17371b0c42744fc8c934c8b95220
kyohei kabasawa <s.life.kb@icloud.com>
2021-11-08 22:37:43 +0900

    No.2

---
+++ tour/imports2.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+}
+
+// factoredインポートステートメントの方がより良いスタイル
+// actored の意味は、「要素化、グループ化、整理済み
